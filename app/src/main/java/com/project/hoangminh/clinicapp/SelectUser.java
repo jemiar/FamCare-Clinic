6549b3827b909579f1d0c2f4f9fbc22efd82f3a8
HoangMinh45 <hhuynh20@uic.edu>
2018-11-11 13:59:34 -0600

    Initial Commit

---
+++ app/src/main/java/com/project/hoangminh/clinicapp/SelectUser.java
@@ -0,0 +1,13 @@
+package com.project.hoangminh.clinicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_user);
+    }
+}
